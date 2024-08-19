893e23a941e092846aa2ea10cded744f36535756
ALNoman78 <abdullaalno078@gmail.com>
2024-08-19 23:26:30 +0600

    featured section styles and responsive desing completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
-
-//   custom styles
-tailwind.config = {
-  theme: {
-    extend: {
-      fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-      },
-    },
-  },
 };
